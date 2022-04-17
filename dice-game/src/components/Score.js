063395ad4c33107a37cfa7cbab4b68d0d3791668
Lacedaimon95 <svetozar95@gmail.com>
2022-04-17 23:15:18 +0200

    #5

--- dice-game/src/components/Score.js
+++ dice-game/src/components/Score.js
@@ -1,12 +1,37 @@
 import React from 'react';
 
-const Score = ({counter1, counter2}) => {
+const Score = ({counter1, counter2, board}) => {
 
 
 
     return ( 
         <>
-         {counter1} : {counter2}
+         <div>
+             <h3>rounds won:</h3>
+             {counter1} : {counter2}
+         </div>
+         <div>
+             <h3>Score:</h3>
+             <table>
+                 <tbody>
+                 <tr>
+                     <td><h2>player 1:</h2></td>
+                     {board.filter((item,index)=>(index%2===0))
+                     .map((item,index)=>(
+                        <td key={index}>{item}</td>
+                     ))}
+                 </tr>
+                 <tr>
+                     <td><h2>player 2:</h2></td>
+                     {board.filter((item,index)=>(index%2!==0))
+                     .map((item,index)=>(
+                         <td key={index}>{item}</td>
+                     ))}
+                 </tr>
+                 </tbody>
+             </table>
+             
+         </div>
         </>
      );
 }
--- dice-game/src/components/Table.js
+++ dice-game/src/components/Table.js
@@ -2,25 +2,37 @@ import React, { useEffect, useState } from 'react';
 import Dice from './Dice';
 import Score from './Score';
 
+
 const Table = () => {
     const number = ["one","two","three","four","five","six"];
 
     const[diceNum, setDiceNum] = useState("one");
     const[diceNumTwo, setDiceNumTwo] = useState("six");
     const[result,setResult] = useState([]);
+    const[board,setBoard] = useState([]);
     let[counter1,setCounter1] = useState(0);
     let[counter2,setCounter2] = useState(0);
 
     const handleRoll = e =>{
         e.preventDefault();
+        let br = 0;
 
-        let index = Math.floor(Math.random()*6);
-        setDiceNum(number[index]);
+        let roll = setInterval(() => {
+            let index = Math.floor(Math.random()*6);
+            setDiceNum(number[index]);
+    
+            let indexTwo = Math.floor(Math.random()*6);
+            setDiceNumTwo(number[indexTwo]);
+            br++;
 
-        let indexTwo = Math.floor(Math.random()*6);
-        setDiceNumTwo(number[indexTwo]);
+            if(br>10){
+                setResult([...result,index + indexTwo + 2])
+                setBoard([...board,index + indexTwo + 2])
+                clearInterval(roll);
+            }
+            
+        }, 100);
 
-        setResult([...result,index + indexTwo + 2])
     }
 
 
@@ -34,9 +46,6 @@ const Table = () => {
             else if (result[1]>result[0]) {
                 setCounter2(counter2+1)
             }
-            else{
-                console.log("=");
-            }
             setResult([]);
         }
 
@@ -44,6 +53,7 @@ const Table = () => {
             setTimeout(() => {
                 setCounter1(0);
                 setCounter2(0);
+                setBoard([]);
             }, 2000);
         }
         
@@ -52,10 +62,12 @@ const Table = () => {
 
     return ( 
         <>
+            {result.length===0?<h3>player 1 turn</h3>:result.length===1?<h3>player 2 turn</h3>:null}
             <Dice diceNum ={diceNum}/>
             <Dice diceNum ={diceNumTwo}/>
             <button onClick={handleRoll}>Roll</button>
-            {counter1===5? <h2>player 1 won!</h2>: counter2===5? <h2>player 2 won!</h2> :<Score counter1={counter1} counter2={counter2}/>}
+            {counter1===5? <h2>player 1 won!</h2>: counter2===5? <h2>player 2 won!</h2> :<Score counter1={counter1} counter2={counter2} board={board}/>}
+
         </> 
     );
 }
