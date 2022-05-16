c0feeb96a7a4fe09ba43ac649c8c568bf8d0b516
Lacedaimon95 <svetozar95@gmail.com>
2022-05-16 19:36:05 +0200

    #10

--- Faster-Truck-webPage/truck-trace-web-page/src/App.js
+++ Faster-Truck-webPage/truck-trace-web-page/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './Components/Navbar';
+import {Home,About,Service,Price,Blog,Contact} from "./Pages/index"
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+
+      <Router>
+        <Navbar/>
+
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/service' element={<Service/>} />
+          <Route path='/price' element={<Price/>} />
+          <Route path='/blog' element={<Blog/>} />
+          <Route path='/contact' element={<Contact/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- Faster-Truck-webPage/truck-trace-web-page/src/Components/Navbar.js
+++ Faster-Truck-webPage/truck-trace-web-page/src/Components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {AiFillInstagram,AiFillLinkedin,AiFillPhone,AiFillYoutube,AiOutlineTwitter,BsFillEnvelopeFill,FaFacebookF} from './index'
+import {Link} from 'react-router-dom'
+import {AiFillInstagram,AiFillLinkedin,AiFillPhone,AiFillYoutube,AiOutlineTwitter,BsFillEnvelopeFill,FaFacebookF,FaTruck,AiOutlineMenu} from './index'
 
 const Navbar = () => {
 
@@ -12,6 +13,18 @@ const Navbar = () => {
                     <article><AiFillPhone/> <span>+381 456 456</span> <span className='crta'>|</span> <BsFillEnvelopeFill/> <span>mail@info.com</span></article>
                     <article><i><FaFacebookF/></i><i><AiOutlineTwitter/></i><i><AiFillLinkedin/></i><i><AiFillInstagram/></i><i><AiFillYoutube/></i></article>
                 </div>
+            </div>
+            <div className='container'>
+                <h2><i><FaTruck/></i> Faster</h2>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/service">Service</Link></li>
+                    <li><Link to="/price">Price</Link></li>
+                    <li><Link to="/blog">Blog</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+
+                </ul>
             </div>
         </nav> 
     );
