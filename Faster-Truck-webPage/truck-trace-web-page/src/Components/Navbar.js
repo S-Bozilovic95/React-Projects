18551ec01b57bd869793f2cb2fba6d32023fbe57
Lacedaimon95 <svetozar95@gmail.com>
2022-05-17 20:10:54 +0200

    8

--- Faster-Truck-webPage/truck-trace-web-page/src/Components/Navbar.js
+++ Faster-Truck-webPage/truck-trace-web-page/src/Components/Navbar.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BiListCheck } from 'react-icons/bi';
 import {Link} from 'react-router-dom'
-import {AiFillInstagram,AiFillLinkedin,AiFillPhone,AiFillYoutube,AiOutlineTwitter,BsFillEnvelopeFill,FaFacebookF,FaTruck,AiOutlineMenu} from './index'
+import {AiFillInstagram,AiFillLinkedin,AiFillPhone,AiFillYoutube,AiOutlineTwitter,BsFillEnvelopeFill,FaFacebookF,FaTruck,AiOutlineMenu,FaShoppingBasket} from './index'
 
 const Navbar = () => {
+    const [show,setShow] = useState(false);
 
+    // const handleDropdown = e =>{
+    //     e.preventDefault();
+    //     setShow(true)
+    // }
+
+    // console.log(show);
 
     
     return ( 
@@ -14,17 +22,21 @@ const Navbar = () => {
                     <article><i><FaFacebookF/></i><i><AiOutlineTwitter/></i><i><AiFillLinkedin/></i><i><AiFillInstagram/></i><i><AiFillYoutube/></i></article>
                 </div>
             </div>
-            <div className='container'>
-                <h2><i><FaTruck/></i> Faster</h2>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
+
+            <div className='navbar container'>
+                <h2><FaTruck/> Faster</h2>
+                <i className='dropDown'><AiOutlineMenu/></i>
+
+                <ul className='list'>
+                    <li><Link to="/" className='lol'>Home</Link></li>
                     <li><Link to="/about">About</Link></li>
                     <li><Link to="/service">Service</Link></li>
                     <li><Link to="/price">Price</Link></li>
                     <li><Link to="/blog">Blog</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
-
+                    <li><i className='basket'><FaShoppingBasket/></i></li>
                 </ul>
+                
             </div>
         </nav> 
     );
