3970ebd02a73a91b8914507ac56423e9edc093a8
Lacedaimon95 <svetozar95@gmail.com>
2022-05-24 20:34:58 +0200

    #13

--- Faster-Truck-webPage/truck-trace-web-page/src/Pages/Contact.js
+++ Faster-Truck-webPage/truck-trace-web-page/src/Pages/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../Components/Header';
+import {MdLocationOn} from 'react-icons/md';
 
 
 
@@ -9,8 +10,26 @@ const Contact = () => {
         <>
         <Header name={"-Contact Us-"}/>
 
-        <section className='contact'>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d19444.477164211!2d21.331666912839403!3d43.576575193270074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ssr!2srs!4v1653159464663!5m2!1ssr!2srs" style={{border:0, width:600,height:450}}   ></iframe>
+        <section className='contact container'>
+        <article>
+            <h3><MdLocationOn style={{color:"white"}}/> 14 Street, Krusevac, Serbia</h3>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d19444.477164211!2d21.331666912839403!3d43.576575193270074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ssr!2srs!4v1653159464663!5m2!1ssr!2srs" style={{border:0, width:400,height:450}}   ></iframe>
+        </article>
+
+        <article>
+            <hgroup>
+                <h4>Contact Us</h4>
+                <h2>Contact For Any Queries</h2>
+            </hgroup>
+
+            <form>
+                <input type="text" placeholder="Your Name" />
+                <input type="text" placeholder="Your Email" />
+                <input type="text" placeholder="Your Subject" />
+                <textarea name='Message' placeholder='Message' cols="30" rows="10" />
+                <input type="submit" value="Send Message"/>
+            </form>
+        </article>
 
         </section>
         </>
