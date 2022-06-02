cb097d4a977e3b6a1679804fae260ee861c26392
Lacedaimon95 <svetozar95@gmail.com>
2022-06-02 15:06:57 +0200

    #20

--- Faster-Truck-webPage/truck-trace-web-page/src/Settings/service/Logistics.js
+++ Faster-Truck-webPage/truck-trace-web-page/src/Settings/service/Logistics.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import { FaPlane, FaShip, FaStore, FaTruck } from 'react-icons/fa';
+import React, { useEffect } from 'react';
+import { FaLeaf, FaPlane, FaShip, FaStore, FaTruck } from 'react-icons/fa';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Logistics = () => {
+
+    useEffect(()=>{
+        AOS.init({
+            duration: 800,
+            easing: 'ease-in-quad',
+            once:false,
+        });
+    },[])
 
     return ( 
         <section className='logistics container1'>
@@ -9,22 +19,22 @@ const Logistics = () => {
             <h2>Best Logistic Services</h2>
 
             <div>
-                <article>
+                <article data-aos="fade-down">
                     <h4> <i><FaPlane/></i> Air Freight</h4>
                     <p>Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet diam sea est diam</p>
                     <a type='button'>Read More</a>
                 </article>
-                <article>
+                <article data-aos="fade-up">
                     <h4> <i><FaShip/></i> Ocean Freight</h4>
                     <p>Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet diam sea est diam</p>
                     <a type='button'>Read More</a>
                 </article>
-                <article>
+                <article data-aos="fade-down">
                     <h4> <i><FaTruck/></i> Land Transport</h4>
                     <p>Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet diam sea est diam</p>
                     <a type='button'>Read More</a>
                 </article>
-                <article>
+                <article data-aos="fade-up">
                     <h4> <i><FaStore/></i> Cargo Storage</h4>
                     <p>Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet diam sea est diam</p>
                     <a type='button'>Read More</a>
