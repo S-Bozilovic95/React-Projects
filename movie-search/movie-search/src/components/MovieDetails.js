a206aecd9a7cac183583ad09baa7c6cc374fd23a
Lacedaimon95 <svetozar95@gmail.com>
2022-06-20 18:45:53 +0200

    #30

--- movie-search/movie-search/src/components/MovieDetails.js
+++ movie-search/movie-search/src/components/MovieDetails.js
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import API from '../assets/api';
 import { ApiKey } from '../assets/ApiKey';
-
+import {GiPopcorn} from 'react-icons/gi';
+import {FaRegClock} from 'react-icons/fa';
+import imdbPic from '../img/imdbLogo.png';
+import rottenPic from '../img/rotten.png';
+import metaPic from '../img/metaCritic.png';
 
 const MovieDetails = () => {
     const[details,setDetails] = useState({
@@ -16,7 +20,7 @@ const MovieDetails = () => {
     const {data,loading,error} = details;
 
     const getDetails= async (imdbID) =>{
-        await API.get(`?i=${imdbID}&${ApiKey}`)
+        await API.get(`?i=${imdbID}&plot=full&${ApiKey}`)
         .then(res=>{
            if(res.data.Response === "True"){
                setDetails({
@@ -41,20 +45,43 @@ const MovieDetails = () => {
 
     console.log(details);
 
-    const handleBack = e =>{
+    const handleBack = () =>{
         navigate("/");
+    }
+
+    const handleRatings = arr =>{
+       return arr.map((el,index)=>{
+            return (<div key={index}>{el.Source==="Rotten Tomatoes"?<img src={rottenPic} />:el.Source==="Internet Movie Database"?<img src={imdbPic}/>:el.Source==="Metacritic"?<img src={metaPic} />:<GiPopcorn/>} <p>{el.Value}</p></div>)
+        })
     }
     
     return ( 
-        <>
-        <h1>{data.Title}</h1>
-        <img src={data.Poster}/>
-        <h3>{data.imdbRating}</h3>
-        <h4>{data.Genre} ({data.Year})</h4>
-        {data.Type ==="series"?<h4>Seasons: {data.totalSeasons}</h4>:<h4>Movie</h4>}
-        <p>{data.Plot}</p>
-        <button onClick={handleBack}>Back</button>
-        </>
+        <section className='detail'>
+                <article style={{backgroundImage:`linear-gradient(to top,rgba(18, 22, 33, 2),rgba(18, 22, 33, 0.5)), url(${data.Poster})`}} className='detail__banner'>
+                    <h1>{data.Title}</h1>
+                </article>
+
+               <div className='container'>
+                    <article className='detail__container__ratings'>
+                        {data.Ratings ? handleRatings(data.Ratings):null}
+                    </article>
+
+                    <article className='detail__container__descShort'>
+                        <h4 className='detail__container__descShort__type'>{data.Type ==="series"? `Seasons: ${data.totalSeasons}`:"Movie"}</h4>
+                        <h4>{data.Genre}</h4>
+                        <h4>{data.Country} - {data.Released}</h4>
+                        <p><i><FaRegClock/></i> {data.Runtime}</p>
+                    </article>
+
+                    <article className='detail__container__descLong'>
+                        <h4>Cast: {data.Actors}</h4>
+                        <h4>Director: {data.Director}</h4>
+                        <h4>Awards: {data.Awards==="N/A"? null : data.Awards}</h4>
+                        <p>{data.Plot}</p>
+                    </article>
+                    <button onClick={handleBack}>Back</button>
+               </div>
+        </section>
      );
 }
  
--- movie-search/movie-search/src/components/MovieItem.js
+++ movie-search/movie-search/src/components/MovieItem.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const MovieItem = ({movie}) => {
-    console.log(movie);
 
     return ( 
         <>
