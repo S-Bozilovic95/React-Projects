7332d240a69140506f9ba15a193d8250969db3ed
Lacedaimon95 <svetozar95@gmail.com>
2022-06-23 15:46:56 +0200

    #35

--- movie-search/movie-search/src/components/Form.js
+++ movie-search/movie-search/src/components/Form.js
@@ -14,8 +14,8 @@ const Form = ({getInfo}) => {
     return ( 
         <nav className="navbar">
             <form  className='navbar__form'>
-                <input className='navbar__form__input' type="text" placeholder='Search for Movies, TV Series...' onChange={(e)=>setName(e.target.value)}/>
-                <i className='navbar__form__icon' onClick={(e)=>handleMovies(e)}><FaSearch/></i>
+                <input className='navbar__form__input' type="text" placeholder='Search for Movies, TV Series...' onChange={(e)=>setName(e.target.value)} required/>
+                <button className='navbar__form__icon' type='submit' onClick={(e)=>handleMovies(e)}><FaSearch/></button>
             </form>
         </nav>
      );
